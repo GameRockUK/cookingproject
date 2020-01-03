7d092e766c9195ea6315e24927d900c80d0afc77
Ubuntu <ubuntu@ip-172-31-94-41.ec2.internal>
2020-01-03 15:45:31 +0000

    Added connection to MongoDB

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bason.objectid import ObjectID
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'Recipe_Book'
+app.config["MONGO_URI"] = 'mongodb+srv://rootbeer:r00tb33r@myfirstcluster-utt5o.mongodb.net/Recipe_Book?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipe():
+    return render_template("recipes.html", recipe=mongo.db.recipes.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+    
